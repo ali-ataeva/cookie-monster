d7e3a7a6721fd2d25d8beec1edd360de284997fc
Alisa Ataeva <a.606777083e@email.com>
2026-03-27 21:38:27 +0100

    feat: add dynamically changing player levels (placeholder achievement cups for now)

--- src/components/Clicker.vue
+++ src/components/Clicker.vue
@@ -1,22 +1,65 @@
 <script lang="ts" setup>
-import { ref } from "vue";
+import { ref, watchEffect } from "vue";
 import { useCounterStore } from "../counter";
-const badge = ref<string>("./achievement-cup.png")
 const coin = ref<string>("./coin.png")
 
 const storeCounter = useCounterStore();
 function increaseCount() {
   storeCounter.count++;
 }
+//Based on counter value assign display a players level
+const levelsMap = new Map([
+  ["Recruit", "./achievement-cup.png"],
+  ["Demolitionist", "./achievement-cup.png"],
+  ["Saboteur", "./achievement-cup.png"],
+  ["Bombardier", "./achievement-cup.png"],
+  ["StrikeSpecialist", "./achievement-cup.png"],
+  ["BlackOpsAgent", "./achievement-cup.png"],
+  ["Commander", "./achievement-cup.png"],
+  ["LivingLegend", "./achievement-cup.png"]
+]);
+let level = ref<string[] | undefined[]>(["Recruit", "./achievement-cup.png"]);
+
+watchEffect(() => {
+  if (storeCounter.count > 500000000) {
+    level.value[0] = "Living Legend"
+    level.value[1] = levelsMap.get("LivingLegend")
+  }
+  if (storeCounter.count > 250000000) {
+    level.value[0] = "Commander"
+    level.value[1] = levelsMap.get("Commander")
+  }
+  if (storeCounter.count > 100000000) {
+    level.value[0] = "Black Ops Agent"
+    level.value[1] = levelsMap.get("BlackOpsAgent")
+  }
+  if (storeCounter.count > 25000000) {
+    level.value[0] = "Strike Specialist"
+    level.value[1] = levelsMap.get("StrikeSpecialist")
+  }
+  if (storeCounter.count > 100000) {
+    level.value[0] = "Bombardier"
+    level.value[1] = levelsMap.get("Bombardier")
+  }
+  if (storeCounter.count > 2500000) {
+    level.value[0] = "Saboteur"
+    level.value[1] = levelsMap.get("Saboteur")
+  }
+  if (storeCounter.count > 50000) {
+    level.value[0] = "Demolitionist"
+    level.value[1] = levelsMap.get("Demolitionist")
+  }
+});
+
 </script>
 <template>
     <article class="clicker">
         <div>
-          <img class="achievement-badge" :src="badge" alt="">
-          <p>Cadet</p>
+          <img class="achievement-badge" :src="level[1]" alt="">
+          <p>{{level[0]}}</p>
         </div>
         <p class="coin-counter">{{ storeCounter.count }} Coins</p>
-        <button @click="increaseCount" class="click-coin">
+        <button @click="increaseCount()" class="click-coin">
           <img :src="coin" alt="game currency coin">
         </button>
       </article>
