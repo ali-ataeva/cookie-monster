a8c66633daeac69d796f2fe964db509dd95bcdc3
Alisa Ataeva <a.606777083e@email.com>
2026-05-07 19:48:20 +0200

    fix: reloading issue in geoguesser

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,5 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
-import { onMounted } from 'vue';
-import { getGeoData } from './services/geoData';
-import { useGeoDataStore } from './stores/geoDataStore';
-
-const geoDataStore = useGeoDataStore()
-
-onMounted(async() => {
-  const {finalCoords, finalOutlines, finalCountries} = await getGeoData()
-  geoDataStore.coords = finalCoords;
-  geoDataStore.outlines = finalOutlines;
-  geoDataStore.countries = finalCountries;
-})
 </script>
 
 <template>
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,21 @@ import { createPinia } from "pinia";
 import "./style.css";
 import App from "./App.vue";
 import { router } from "./router";
+import { getGeoData } from "./services/geoData";
+import { useGeoDataStore } from "./stores/geoDataStore";
 
 const pinia = createPinia();
 const app = createApp(App);
 app.use(router);
 app.use(pinia);
-app.mount("#app");
+
+const geoDataStore = useGeoDataStore();
+
+(async () => {
+  const { finalCoords, finalOutlines, finalCountries } = await getGeoData();
+  geoDataStore.coords = finalCoords;
+  geoDataStore.outlines = finalOutlines;
+  geoDataStore.countries = finalCountries;
+
+  app.mount("#app");
+})();
